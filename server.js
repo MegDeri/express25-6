e92e262eab477875399c6061e791cf0c3eb58ad1
Magda Aderibigbe <magdaaderibigbe@gmail.com>
2019-07-18 22:21:08 +0300

    Add basic files

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views','./views');
+
+app.use('/store', function(req, res, next){
+    console.log('Jestem pośrednikiem przy żądaniu do /store!');
+   next();
+});
+
+app.get('/', function (req, res) {
+    res.send('Hello world!');
+});
+
+app.get('/store', function(req, res) {
+    res.send('To jest sklep');
+});
+
+app.listen(3000);
+app.use(function (req, res, next) {
+    res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
+});
